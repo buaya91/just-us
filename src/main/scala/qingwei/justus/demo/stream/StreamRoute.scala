83fe6bfb4d228ffa5cce399ceef796644ed47f6f
QingWei <l.q.wei91@gmail.com>
2016-05-14 23:44:22 +0800

    Use time stream instead

--- src/main/scala/qingwei/justus/demo/stream/StreamRoute.scala
+++ src/main/scala/qingwei/justus/demo/stream/StreamRoute.scala
@@ -1,27 +1,31 @@
 package qingwei.justus.demo.stream
 
+import java.io.File
+
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.{ RequestContext, Route, RouteResult }
-import akka.stream.scaladsl.{ Sink, Source }
+import akka.stream.scaladsl.{ FileIO, Sink, Source }
 import akka.http.scaladsl.model.HttpMethods._
 import akka.stream.ActorMaterializer
 import akka.util.ByteString
 import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.Future
-import scala.sys.process.Process
+import scala.concurrent.duration._
 
 trait StreamRoute {
   implicit val materializer: ActorMaterializer
 
   val filePath = ConfigFactory.load().getConfig("demo.stream").getString("path")
   def logStream(): Source[ByteString, Any] = {
-    val tailStream = Process(s"tail -f $filePath").lineStream.map(s => {
-      println(s"stream line: $s")
-      ByteString(s)
-    })
-    Source(tailStream)
+    FileIO.fromFile(new File(filePath)).keepAlive(2 seconds, () => ByteString(s"Server time(ms): ${System.currentTimeMillis()}"))
   }
+
+  def timeStream: Source[ByteString, Any] =
+    Source
+      .repeat(ByteString("10 seconds passed"))
+      .delay(10 seconds)
+      .keepAlive(2 seconds, () => ByteString(s"Server time(ms): ${DateTime.now}"))
 
   def streamHandler: Route = (ctx: RequestContext) => {
     import RouteResult._
@@ -30,7 +34,7 @@ trait StreamRoute {
         println("receive req")
         entity.dataBytes.runWith(Sink.ignore)
         Future.successful(
-          Complete(HttpResponse(entity = HttpEntity.Chunked.fromData(ContentTypes.`text/plain(UTF-8)`, logStream())))
+          Complete(HttpResponse(entity = HttpEntity.Chunked.fromData(ContentTypes.`text/plain(UTF-8)`, timeStream)))
         )
       }
       case HttpRequest(_, _, _, entity, _) => {
