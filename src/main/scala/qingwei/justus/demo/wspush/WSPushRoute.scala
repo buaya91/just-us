2a4676b5f6b27fe296d723f22d7c895fd05a0312
QingWei <l.q.wei91@gmail.com>
2016-06-11 13:40:46 +0800

    Add end msg

--- src/main/scala/qingwei/justus/demo/wspush/WSPushRoute.scala
+++ src/main/scala/qingwei/justus/demo/wspush/WSPushRoute.scala
@@ -2,10 +2,10 @@ package qingwei.justus.demo.wspush
 
 import akka.NotUsed
 import akka.actor.Props
-import akka.http.scaladsl.model.ws.{Message, TextMessage}
+import akka.http.scaladsl.model.ws.{ Message, TextMessage }
 import akka.http.scaladsl.server.Directives._
 import akka.stream.FlowShape
-import akka.stream.scaladsl.{Flow, GraphDSL, Sink, Source}
+import akka.stream.scaladsl.{ Flow, GraphDSL, Sink, Source }
 
 trait WSPushRoute {
   def serverPushHandler: Flow[Message, Message, NotUsed] = Flow.fromGraph(GraphDSL.create() { implicit b =>
