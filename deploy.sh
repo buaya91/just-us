c5db883f350e968da152f1c042fb860722468f4f
QingWei <l.q.wei91@gmail.com>
2016-07-30 17:44:15 +0800

    Update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 activator assembly &&
 mv target/scala-2.11/just-us-assembly-1.0.jar dist/ &&
-scp -r dist root@128.199.204.38:just-us/
+scp -r dist root@128.199.77.84:/root/just-us/backend/
